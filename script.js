5ea627fabef4c2308cc08d65da995a86c4b9e3cf
Katjahkrogh <you@example.com>
2023-10-03 15:44:26 +0200

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+// gsap.matchMedia().add("(prefers-reduced-motion: no-preference)", () => {
+//   const tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: ".header-img",
+//       start: "top 50%",
+//       end: "+=600px",
+//       scrub: true,
+//     },
+//   });
+
+//   tl.to(".header-img", {
+//     y: 0,
+//   }).to(".header-img", {
+//     y: 500,
+//   });
+// });
